2102cda2b3abcfbda2cac0794c7f0cc90e2b39a6
Joseph Savold <joesavold@hotmail.com>
2017-03-28 14:33:50 -0400

    Fixed accidental double copy

--- xmlSpec/deploy/README.md
+++
@@ -1,4 +0,0 @@
-# 416Project3
-Animation in C++ with SDL 2.0.
-
-All sprites and animations were created by Joseph Savold. Tracker framework was supplied by Dr. Malloy. 
--- xmlSpec/deploy/gameObject.cpp
+++
@@ -1,6 +0,0 @@
-#include "gameObject.h"
-
-bool GameObject::inBounds(int x, int y, int w, int h) {
-	if (x < 0-w || x > WIDTH || y < 0-h || y > HEIGHT) return false;
-	return true;
-}
